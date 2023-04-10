675beff03cc58e09b5f21b27f5a228812a483915
Lizanne Cerami <lizanne.cerami@gmail.com>
2023-04-09 21:11:37 -0400

    added quotes

--- JSON-object.js
+++ JSON-object.js
@@ -1,39 +1,39 @@
 myObject = {
   "full name": "Lizanne Cerami",
-  age: "36",
+  "age": "34",
   "favorite books": [
     {
-      title: "The Perks of Being a Wallflower",
-      author: "Stephen Chbosky",
+      "title": "The Perks of Being a Wallflower",
+      "author": "Stephen Chbosky",
       "favorite quote":
         "Maybe it's good to put things in perspective, but sometimes, I think that the only perspective is to really be there. Because it's okay to feel things. I was really there. And that was enough to make me feel infinite. I feel infinite.",
     },
     {
-      title: "It Ends With Us",
-      author: "Colleen Hoover",
+      "title": "It Ends With Us",
+      "author": "Colleen Hoover",
       "favorite quote": "It stops here. With me and you. It ends with us.",
     },
     {
-      title: "Hopeless",
-      author: "Colleen Hoover",
+      "title": "Hopeless",
+      "author": "Colleen Hoover",
       "favorite quote":
         "The events of your life are all crammed together one minute right after the other without any time lapses or blank pages or chapter breaks because no matter what happens life just keeps going and moving forward and words keep flowing and truths keep spewing whether you like it or not and life never lets you pause and just catch your fucking breath.",
     },
     {
-      title: "Reason to Breathe - The Breathing Series",
-      author: "Rebecca Donovan",
+      "title": "Reason to Breathe - The Breathing Series",
+      "author": "Rebecca Donovan",
       "favorite quote":
         "It was better to try and fail, than to never have the experience",
     },
     {
-      title: "Kingdom of Ash - Throne of Glass Series",
-      author: "Sarah J. Maas",
+      "title": "Kingdom of Ash - Throne of Glass Series",
+      "author": "Sarah J. Maas",
       "favorite quote":
         "But just remember that this fear of yours? It means you have something worth fighting for – something you care so greatly for that losing it is the worst thing you can imagine.",
     },
     {
-      title: "Maybe Someday",
-      author: "Colleen Hoover",
+      "title": "Maybe Someday",
+      "author": "Colleen Hoover",
       "favorite quote":
         "Sometimes in life, we need a few bad days in order to keep the good ones in perspective.",
     },
